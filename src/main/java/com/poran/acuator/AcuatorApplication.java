d8c78a8921296102bfc1a68429c3ad3ba8f01497
poran200 <chowdury659@gmail.com>
2021-02-14 03:30:10 +0600

    Initial commit

---
+++ src/main/java/com/poran/acuator/AcuatorApplication.java
@@ -0,0 +1,13 @@
+package com.poran.acuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AcuatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/poran/acuator/AcuatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poran.acuator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcuatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
